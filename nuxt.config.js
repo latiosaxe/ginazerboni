823be5c56843810eb12f28348066e99026a00865
Axel <latiosaxe@gmail.com>
2018-09-02 14:09:08 -0500

    Cooming soon

---
+++ nuxt.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Gina Zerboni | EDUKOACHING',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Sitio personal de Gina Zerboni' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#246792' },
+  /*
+  ** Build configuration
+  */
+
+  css: [
+    { src: '~assets/styles/main.scss', lang: 'scss' }
+  ],
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="soon_wrapper" :class="{ 'active': active }">
+    <img src="~static/logo_final.png" alt="Gina Zerboni">
+    <p>Por el momento mi sitio se encuentra en construcción, pero estará listo muy pronto</p>
+    <p>Si deseas ponerte en contacto conmigo puedes mandarme un correo a <a href="mailto:gina.zerboni@gmail.com">gina.zerboni@gmail.com</a>.</p>
+  </section>
+</template>
+
+<script>
+// import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  data () {
+    return {
+      active: false
+    }
+  },
+  mounted() {
+    //do something after mounting vue instance
+    setTimeout(function(){
+      this.active = true;
+    }.bind(this), 500)
+  },
+  components: {
+    // AppLogo
+  }
+}
+</script>
